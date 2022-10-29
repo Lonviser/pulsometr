a4bf2940317bc20312b05f97e6bbc58f1da58edc
Lonviser <47rex47@mail.ru>
2022-10-29 18:55:28 +0300

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-import 'normalize.css';
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../imgs/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../imgs/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots:true,
+                arrow:false,
+              }
+            }
+          ]
+        });
+    });
+
