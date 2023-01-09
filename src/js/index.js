f7477c5fd0522f1e011388248c3f81ac24765d01
Lonviser <47rex47@mail.ru>
2023-01-09 18:43:10 +0300

    add card

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../imgs/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../imgs/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="imgs/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="imgs/right.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
