64623f4780b0f0aaa24103f5a51d77e5521e402f
Lonviser <47rex47@mail.ru>
2023-01-13 12:13:34 +0300

    add css animation

--- src/js/index.js
+++ src/js/index.js
@@ -83,8 +83,62 @@ validateForms('#consultation-form');
 validateForms('#consultation form');
 validateForms('#order form');
 
-  $('input[name=phone]').mask("+375 (99) 999-99-99")
+  $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+  $('form').submit(function(e){
+    e.preventDefault();
+
+    if(!(this).valid()){
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find("input").val("");
+
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 
 });
 
+//smooth scroll and pageup
 
+$(window).scroll(function(){
+  if($(this).scrollTop()>1600){
+    $('.pageup').fadeIn();
+  }
+  else{
+    $('.pageup').fadeOut();
+  }
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".pageup").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
